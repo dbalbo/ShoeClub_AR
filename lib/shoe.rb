8cf9ab56567641c2cba4f232edcf7bfae39b71cd
debbie <debbalbo@hotmail.com>
2015-06-19 09:00:13 -0700

    adding capitilazation callback in a private method to be able to have any input be capitalized no matter how it is entered into the form

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,12 @@
 class Shoe < ActiveRecord::Base
 	has_and_belongs_to_many :brands
 	validates(:style, :presence => true)
+	before_save(:titlecase_name)
+
+
+	private
+
+	def titlecase_name
+		self.name = (name.titlecase)
+	end
 end
--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -3,4 +3,5 @@ require('spec_helper')
 describe(Shoe) do
 	it { should have_and_belong_to_many (:brands) }
 	it { should allow_value('style').for(:style) }
+	it { should callback(:titlecase_name).before(:save) }
 end
