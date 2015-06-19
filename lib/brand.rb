e3f7c5f61eacf28d79ff8b81fe547f62d48e284b
debbie <debbalbo@hotmail.com>
2015-06-19 09:02:53 -0700

    validating presence of name for brand class

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,4 @@
 class Brand < ActiveRecord::Base
 	has_and_belongs_to_many :shoes
+	validates(:name, :presence => true)
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,4 +2,5 @@ require('spec_helper')
 
 describe(Brand) do
 	it { should have_and_belong_to_many (:shoes) }
+	it { should validate_presence_of(:name) }
 end
