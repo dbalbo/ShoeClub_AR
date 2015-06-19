8326e839dbf49c6083e74fc26cb7a9214cab2b4e
debbie <debbalbo@hotmail.com>
2015-06-19 08:57:13 -0700

    validating presence of style for shoe class

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
 class Shoe < ActiveRecord::Base
 	has_and_belongs_to_many :brands
+	validates(:style, :presence => true)
 end
--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -2,4 +2,5 @@ require('spec_helper')
 
 describe(Shoe) do
 	it { should have_and_belong_to_many (:brands) }
+	it { should allow_value('style').for(:style) }
 end
